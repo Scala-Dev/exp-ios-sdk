8ab8943e20540858b152e39fdd88dbd2e6acfdf1
Cesar Oyarzun <cesar.oyarzun@scala.com>
2016-04-25 15:52:19 -0400

    EXP-1638 add missing field key to zone

--- Pod/Classes/Zone.swift
+++ Pod/Classes/Zone.swift
@@ -12,12 +12,15 @@ import Foundation
 public final class Zone: Model,ResponseObject,ResponseCollection {
 
     public let name: String
+    public let key: String
     
     required public init?(response: NSHTTPURLResponse, representation: AnyObject) {
         if let representation = representation as? [String: AnyObject] {
             self.name = representation["name"] as! String
+            self.key = representation["key"] as! String
         } else {
             self.name = ""
+            self.key = ""
         }
         
         super.init(response: response, representation: representation)
