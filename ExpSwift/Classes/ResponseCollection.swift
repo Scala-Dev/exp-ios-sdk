477655ce95930bfa3a0ee8829eb26468242f592e
Cesar Oyarzun <cesar.oyarzun@scala.com>
2016-10-25 14:37:54 -0400

    EXP-3356 fix breaking changes for response and response collection serializable

--- ExpSwift/Classes/ResponseCollection.swift
+++ ExpSwift/Classes/ResponseCollection.swift
@@ -11,42 +11,49 @@ import Alamofire
 
 
 public protocol ResponseCollection {
-    static func collection(response: HTTPURLResponse, representation: AnyObject) -> [Self]
+    static func collection(from response: HTTPURLResponse, withRepresentation representation: Any) -> [Self]
 }
 
-extension Alamofire.Request {
-    public func responseCollection<T: ResponseCollection>(_ completionHandler: (Response<SearchResults<T>, NSError>) -> Void) -> Self {
+extension ResponseCollection where Self: ResponseObject {
+    static func collection(from response: HTTPURLResponse, withRepresentation representation: Any) -> [Self] {
+        var collection: [Self] = []
         
-        let responseSerializer = ResponseSerializer<SearchResults<T>, NSError> { request, response, data, error in
-            guard error == nil else { return .Failure(error!) }
-            
-            let JSONSerializer = Request.JSONResponseSerializer(options: .AllowFragments)
-            let result = JSONSerializer.serializeResponse(request, response, data, error)
-            
-            switch result {
-            case .Success(let value):
-                if let response = response {
-                    let dic = value as! NSDictionary
-                    if let _ = dic.objectForKey("code") as? String{
-                        let failureReason = dic.objectForKey("message") as! String
-                        let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
-                        return .Failure(error)
-                    }
-                    let results: AnyObject? = dic.objectForKey("results")
-                    let total: Int64 = (dic.objectForKey("total") as! NSNumber).longLongValue
-                    let collection: [T] = T.collection(response: response, representation: results!)
-                    let searchResults = SearchResults<T>(results: collection, total: total)
-                    return .Success(searchResults!)
-                } else {
-                    let failureReason = "Response collection could not be serialized due to nil response"
-                    let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
-                    return .Failure(error)
+        if let representation = representation as? [[String: Any]] {
+            for itemRepresentation in representation {
+                if let item = Self(response: response, representation: itemRepresentation) {
+                    collection.append(item)
                 }
-            case .Failure(let error):
-                return .Failure(error)
             }
         }
-        return response(responseSerializer: responseSerializer, completionHandler: completionHandler)
+        
+        return collection
+    }
+}
 
+extension DataRequest {
+    @discardableResult
+    func responseCollection<T: ResponseCollection>(
+        queue: DispatchQueue? = nil,
+        completionHandler: @escaping (DataResponse<[T]>) -> Void) -> Self
+    {
+        let responseSerializer = DataResponseSerializer<[T]> { request, response, data, error in
+            guard error == nil else { return .failure(BackendError.network(error: error!)) }
+            
+            let jsonSerializer = DataRequest.jsonResponseSerializer(options: .allowFragments)
+            let result = jsonSerializer.serializeResponse(request, response, data, nil)
+            
+            guard case let .success(jsonObject) = result else {
+                return .failure(BackendError.jsonSerialization(error: result.error!))
+            }
+            
+            guard let response = response else {
+                let reason = "Response collection could not be serialized due to nil response."
+                return .failure(BackendError.objectSerialization(reason: reason))
+            }
+            
+            return .success(T.collection(from: response, withRepresentation: jsonObject))
+        }
+        
+        return response(responseSerializer: responseSerializer, completionHandler: completionHandler)
     }
 }
--- ExpSwift/Classes/ResponseObject.swift
+++ ExpSwift/Classes/ResponseObject.swift
@@ -11,41 +11,40 @@ import Alamofire
 
 
 public protocol ResponseObject {
-    init?(response: HTTPURLResponse, representation: AnyObject)
+    init?(response: HTTPURLResponse, representation: Any)
 }
 
-extension Request {
- public func responseObject<T: ResponseObject>(_ completionHandler: (Response<T, NSError>) -> Void) -> Self {
-        
-        let responseSerializer = ResponseSerializer<T, NSError> { request, response, data, error in
-            guard error == nil else { return .Failure(error!) }
-            let JSONResponseSerializer = Request.JSONResponseSerializer(options: .AllowFragments)
-            let result = JSONResponseSerializer.serializeResponse(request, response, data, error)
+enum BackendError: Error {
+    case network(error: Error) // Capture any underlying Error from the URLSession API
+    case dataSerialization(error: Error)
+    case jsonSerialization(error: Error)
+    case xmlSerialization(error: Error)
+    case objectSerialization(reason: String)
+}
+
+extension DataRequest {
+    func responseObject<T: ResponseObject>(
+        queue: DispatchQueue? = nil,
+        completionHandler: @escaping (DataResponse<T>) -> Void)
+        -> Self
+    {
+        let responseSerializer = DataResponseSerializer<T> { request, response, data, error in
+            guard error == nil else { return .failure(BackendError.network(error: error!)) }
             
-            switch result {
-            case .Success(let value):
-                if let dic = value as? NSDictionary{
-                    if let _ = dic.objectForKey("code") as? String{
-                        let failureReason = dic.objectForKey("message") as! String
-                        let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
-                        return .Failure(error)
-                    }
-                }
-                if let response = response,
-                    let responseObject = T(response: response, representation: value)
-                {
-                    return .Success(responseObject)
-                } else {
-                    let failureReason = "JSON could not be serialized into response object: \(value)"
-                    let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
-                    return .Failure(error)
-                }
-            case .Failure(let error):
-                return .Failure(error)
+            let jsonResponseSerializer = DataRequest.jsonResponseSerializer(options: .allowFragments)
+            let result = jsonResponseSerializer.serializeResponse(request, response, data, nil)
+            
+            guard case let .success(jsonObject) = result else {
+                return .failure(BackendError.jsonSerialization(error: result.error!))
             }
+            
+            guard let response = response, let responseObject = T(response: response, representation: jsonObject) else {
+                return .failure(BackendError.objectSerialization(reason: "JSON could not be serialized: \(jsonObject)"))
+            }
+            
+            return .success(responseObject)
         }
         
-        return response(responseSerializer: responseSerializer, completionHandler: completionHandler)
+        return response(queue: queue, responseSerializer: responseSerializer, completionHandler: completionHandler)
     }
 }
-
